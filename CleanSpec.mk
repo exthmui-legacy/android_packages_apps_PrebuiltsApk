700784fc991eb1673470d7f40286df7d0edafa91
HearnYuyuko <dingwenxuan4@gmail.com>
2021-12-05 04:14:47 +0000

    PrebuiltsApk: Fetch from Arrow and add ViaBrowser
    Change-Id: Ie833d27804ae62d6873b7945e960f863bbdb88f8

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -1,6 +1,4 @@
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/DuckDuckGo)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/ViaBrowser)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/SimpleGallery)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/SimpleCalender)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/stats)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/WeatherIcons)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/Lawnchair)
